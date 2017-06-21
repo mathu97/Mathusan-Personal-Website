b7b5a67c316ebe334456a6e6aca0ebe14ff823f1
Mathusan Selvarajah <mathusans52@gmail.com>
2017-06-21 10:21:52 -0400

    Intro Animation Fixes

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 function introAnimation(){
     Typed.new('.element h1', {
-        strings: [ "CS STUDENT", "SOFTWARE ENGINEER", "ENTREPRENEUR"],
+        strings: [ "CS STUDENT", "SOFTWARE ENGINEER", "ENTREPRENEUR", ""],
         typeSpeed: 0,
         startDelay: 1500,
         backDelay: 600,
         callback: function() {
-            postIntroAnimation(finalIntroAnimation())
+            postIntroAnimation(finalIntroAnimation)
         }
     });
 }
 
 function postIntroAnimation(callback){
     $(".animate-text").fadeOut(2000);
+    callback()
 }
 
 function finalIntroAnimation(){
-    console.log("Enters")
     $(".animate-text").css({
       "display": "none"
     });
-
-    $(".main-text").fadeIn(2000, function(){
-      console.log("Fading In")
-    })
+    $(".main-text").fadeIn(2000)
 }
 
 $(function(){
