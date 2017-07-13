0718722ddf729efa9f927d9bd5f546303550d7f1
Mathusan Selvarajah <mathusans52@gmail.com>
2017-07-12 22:30:18 -0400

    Fixing Carousel animation bug

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function introAnimation(){
+  //Animation that loops through a couple words
     Typed.new('.element h1', {
         strings: [ "CS STUDENT", "IOS DEVELOPER", "WEB DEVELOPER", "SOFTWARE ENGINEER", ""],
         typeSpeed: 0,
@@ -11,11 +12,13 @@ function introAnimation(){
 }
 
 function postIntroAnimation(callback){
+  //Makes the typed intro fade away
     $(".animate-text").fadeOut(2000);
     callback()
 }
 
 function finalIntroAnimation(){
+    //Fades in the text for the main page
     $(".animate-text").css({
       "display": "none"
     });
@@ -33,7 +36,7 @@ function isScrolledIntoView(elem){
 }
 
 function navbarFunctions(){
-
+  //Adds action listeners to the navbar buttons
   $('#about-me').click(function(){
     $('html, body').animate({
         scrollTop: $('.aboutMe').offset().top
@@ -49,12 +52,14 @@ function navbarFunctions(){
 }
 
 function animateCarouselHeadings(){
+  //Animates the carousel headings
   var currentIndex = $('div.active').index() + 1;
+  console.log(currentIndex)
   var heading = ""
   if (currentIndex == 1)
-     heading = "EXPERIENCE"
-  else if (currentIndex == 2)
      heading = "EDUCATION"
+  else if (currentIndex == 2)
+     heading = "EXPEREINCE"
 
   Typed.new('.carousel-inner section h1', {
       strings: [heading],
@@ -69,8 +74,16 @@ function animateCarouselHeadings(){
 $(function(){
   $(window).on("load", function() {
     introAnimation()
-    animateCarouselHeadings()
-    
+
+    //Type "EXPERIENCE" onto the first carousel
+    Typed.new('.carousel-inner section h1', {
+        strings: ["EXPEREINCE"],
+        typeSpeed: 0,
+        startDelay: 500,
+        backDelay: 600,
+        callback: function() {}
+    });
+
     $('.page-down').click(function(){
        var nextSection = $(this).closest('.scrolling-section').next('.scrolling-section');
        $('html, body').animate({
